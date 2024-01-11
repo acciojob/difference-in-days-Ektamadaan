4ed9f887a7da4e373f7a9b562b52ade7fee59f90
Ektamadaan <ektamadaan45@gmail.com>
2024-01-11 22:09:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var dateDiffInDays = function (date1, date2) {
 	const parsedate1 = new Date(date1);
 	const parsedate2 = new Date(date2);
 
-	const diffinsec = Math.abs(parsedate1 - parsedate2);
+	const diffinsec = parsedate2 - parsedate1;
 	const diffindays = Math.round(diffinsec/(1000*60*60*24));
 
 	return diffindays;
